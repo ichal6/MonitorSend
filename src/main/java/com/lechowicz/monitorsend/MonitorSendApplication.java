a590413ded4248d1b07fbef0f7c1be6da8c95ef4
Michael Lechowicz <ichal.lechowicz@gmail.com>
2021-02-08 18:44:47 +0100

    Initial commit

---
+++ src/main/java/com/lechowicz/monitorsend/MonitorSendApplication.java
@@ -0,0 +1,13 @@
+package com.lechowicz.monitorsend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorSendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonitorSendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lechowicz/monitorsend/MonitorSendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lechowicz.monitorsend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorSendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
